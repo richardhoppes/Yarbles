5eb62d4034772078ffae991f787129509f7cf320
Richard Hoppes <rhoppes@zappos.com>
2012-11-21 13:48:56 -0800

    changed app url to yarbles.richardhoppes.com, fixed heartbeat controller

--- application/yarbles/web/controller/HeartbeatController.php
+++ application/yarbles/web/controller/HeartbeatController.php
@@ -1,10 +1,11 @@
 <?php
 namespace yarbles\web\controller;
 
+use yarbles\framework\Controller;
 use yarbles\framework\helper\AppVersionHelper;
 use yarbles\framework\helper\FrameworkVersionHelper;
 
-class HeartbeatController {
+class HeartbeatController extends Controller {
 
 	public function main() {
 		$arrDisplay = array(
